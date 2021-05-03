ce0c330fe5b8b5771a8b3c6042055cc0a64b8833
sachin-kumar <skumar182@sapient.com>
2021-05-03 18:11:35 +0100

    snapshot updated

--- src/modules/Grid-row/grid-row.test.js
+++ src/modules/Grid-row/grid-row.test.js
@@ -5,38 +5,29 @@ import Adapter from 'enzyme-adapter-react-16';
 import GridRow from './grid-row';
 
 Enzyme.configure({ adapter: new Adapter() });
-const data = {
-  response: {
-    map: {
-      x: 1611,
-      y: 16,
-    },
-    lostCell: {
-      x: [],
-      y: [],
-    },
-    robots: [
-      {
-        name: 'R1',
-        color: 'green',
-        currentPosition: '0 0 N',
-        x: 0,
-        y: 1,
-        d: 'N',
-        instructions: 'FFFFFFFFFFFFFFFFFFF',
-      },
-      {
-        name: 'R2',
-        color: 'green',
-        currentPosition: '0 14 N',
-        x: 0,
-        y: 1,
-        d: 'N',
-        instructions: 'FRFFFFFFF',
-      },
-    ],
+
+const data = [
+  {
+    name: 'R1',
+    color: 'green',
+    currentPosition: '0 0 N',
+    grid: '50 50',
+    x: 0,
+    y: 1,
+    d: 'N',
+    instructions: 'FFFFFFFFFFFFFFFFFFF',
   },
-};
+  {
+    name: 'R2',
+    color: 'green',
+    grid: '3 3',
+    currentPosition: '0 14 N',
+    x: 0,
+    y: 1,
+    d: 'N',
+    instructions: 'FRFFFFFFF',
+  },
+];
 
 jest.mock('../../context/marsContext', () => {
   return {
