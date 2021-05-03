3dc5cda6039f6f09f3941ef922c60a687918c7d7
sachin-kumar <skumar182@sapient.com>
2021-05-03 10:18:12 +0100

    Planet component path correction

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Planet from './Planet';
+import { Planet } from './components/';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
