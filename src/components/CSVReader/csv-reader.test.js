1d81f1ae02126915474ae732cdd262e4254ff1c2
sachin-kumar <skumar182@sapient.com>
2021-05-04 02:58:44 +0100

    ** Unit test added

---
+++ src/components/CSVReader/csv-reader.test.js
@@ -0,0 +1,51 @@
+import { render, screen, cleanup } from '@testing-library/react';
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import CSVReaderComponent from './csv-reader';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const data = {
+  dimension: {
+    xaxis: 1,
+    yaxis: 1,
+  },
+  robots: [
+    {
+      name: 'R1',
+      color: 'green',
+      currentPosition: '0 0 N',
+      grid: '50 50',
+      x: 0,
+      y: 1,
+      d: 'N',
+      instructions: 'FFFFFFFFFFFFFFFFFFF',
+    },
+    {
+      name: 'R2',
+      color: 'green',
+      grid: '3 3',
+      currentPosition: '0 14 N',
+      x: 0,
+      y: 1,
+      d: 'N',
+      instructions: 'FRFFFFFFF',
+    },
+  ],
+};
+
+jest.mock('../../context/marsContext', () => {
+  return {
+    useMarsContextAPI: () => data,
+  };
+});
+
+describe('CSV reader snapshot', () => {
+  afterEach(cleanup);
+
+  it('should take a snapshot', () => {
+    const { asFragment } = render(<CSVReaderComponent />);
+    expect(asFragment(<CSVReaderComponent />)).toMatchSnapshot();
+  });
+});
